35249e5eae1192dc7dee3db6b60bb16179db9284
Bruno Falcão <109874090+brunofalcao89@users.noreply.github.com>
2022-07-26 15:57:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS-curso-em-video
+ Curso do JS do curso em video
---
+++ aula16/funcao01.js
@@ -0,0 +1,9 @@
+function parimp(n){
+    if(n%2==0){// se N é divisivel por  2 com resto 0.
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+console.log(parimp(4))
